e0815405b3358da1b210e77a69f4cf5c3f02236c
elwakeel91 <37563049+elwakeel91@users.noreply.github.com>
2018-03-20 02:09:30 +0000

    Add files via upload
    You are choreograhing a circus show with various animals. For one act, you are given two kangaroos on a number line ready to jump in the positive direction (i.e, toward positive infinity).
    
    The first kangaroo starts at location (x1) and moves at a rate of (v1) meters per jump.
    The second kangaroo starts at location (x2) and moves at a rate of (v2) meters per jump.
    You have to figure out a way to get both kangaroos at the same location as part of the show.
    
    Complete the function kangaroo which takes starting location and speed of both kangaroos as input, and return "YES" or "NO" appropriately. Can you determine if the kangaroos will ever land at the same location at the same time? The two kangaroos must land at the same location after making the same number of jumps.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+    // solve using the equation:
+    // Number of jumps = (x1 - x2) / (v2 - v1)
+
+    static string Kangaroo(int x1, int v1, int x2, int v2)
+    {
+        // Check if they are already at the same position
+        if (x1 == x2)
+            return "YES";
+
+        // Find the number of jumps
+        float numberOfJumps = (float)(x1 - x2) / (v2 - v1);
+
+        // Check that the number of jumps is positive and we don't have a remainder
+        if (numberOfJumps >= 0 && (numberOfJumps % 1) == 0)
+            return "YES";
+        else
+            return "NO";
+    }
+
+    static void Main(String[] args)
+    {
+        string[] tokens_x1 = Console.ReadLine().Split(' ');
+        int x1 = Convert.ToInt32(tokens_x1[0]);
+        int v1 = Convert.ToInt32(tokens_x1[1]);
+        int x2 = Convert.ToInt32(tokens_x1[2]);
+        int v2 = Convert.ToInt32(tokens_x1[3]);
+        string result = Kangaroo(x1, v1, x2, v2);
+        Console.WriteLine(result);
+    }
+}

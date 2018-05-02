139a18fab81d02aabce2ba2e533eeede07ce1f76
Kareem El-Wakeel <elwakeel91@gmail.com>
2018-05-02 04:44:38 +0100

    Add Angry Professor problem Unit Test

---
+++ Algorithms/Implementation/Angry_Professor/Angry_Professor_UnitTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using HackerRank_CSharp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRank.UnitTests
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void AngryProfessor_LessonIsCancelled_ReturnsYes()
+        {
+            // Arrange
+            int minimumNumberOnTime = 3;
+            int numberOfStudents = 4;
+            int[] arrivalTimes = new int[] { -2, -3, 2, 1 };
+
+            // Act
+            var result = Solution.angryProfessor(numberOfStudents, minimumNumberOnTime, arrivalTimes);
+
+            // Assert
+            Assert.AreEqual(result, "YES");
+        }
+
+        [TestMethod]
+        public void AngryProgessor_LessonIsNotCancelled_ReturnsNo()
+        {
+            // Arrange
+            int numberOfStudents = 4;
+            int minimumNumberOnTime = 3;
+            int[] arrivalTimes = new int[] { -4, -2, 0, 1 };
+
+            // Act
+            var result = Solution.angryProfessor(numberOfStudents, minimumNumberOnTime, arrivalTimes);
+
+            // Assert
+            Assert.AreEqual(result, "NO");
+        }
+    }
+}

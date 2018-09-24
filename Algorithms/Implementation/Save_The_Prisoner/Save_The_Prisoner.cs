57130ea1089b6e0c6da31cd86544fbc4097be6ab
Kareem El-Wakeel <elwakeel91@gmail.com>
2018-09-24 11:56:35 +0100

    Add Save The Prisoner problem

---
+++ Algorithms/Implementation/Save_The_Prisoner/Save_The_Prisoner.cs
@@ -0,0 +1,45 @@
+﻿// URL of the problem
+// https://www.hackerrank.com/challenges/save-the-prisoner/problem
+
+using System;
+using System.IO;
+
+public class Solution
+{
+	/// <summary>
+    /// Calculates the last prisoner to get candy
+    /// </summary>
+    /// <param name="prisoners">The number of prisoners</param>
+    /// <param name="sweets">The amount of sweets to hand out</param>
+    /// <param name="startingSeat">The starting seat to hand out from</param>
+    /// <returns></returns>
+    public static int SaveThePrisoner(int prisoners, int sweets, int startingSeat)
+    {
+        return ((sweets - 1 + startingSeat - 1) % prisoners) + 1;
+    }
+
+    static void Main(String[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int t = Convert.ToInt32(Console.ReadLine());
+
+        for (int tItr = 0; tItr < t; tItr++)
+        {
+            string[] nms = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nms[0]);
+
+            int m = Convert.ToInt32(nms[1]);
+
+            int s = Convert.ToInt32(nms[2]);
+
+            int result = SaveThePrisoner(n, m, s);
+
+            textWriter.WriteLine(result);
+        }
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
---
+++ Algorithms/Implementation/Save_The_Prisoner/Save_The_Prisoner_UnitTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRank.UnitTests
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        /// <summary>
+        /// Given 5 prisoners, 2 sweets and starting seat 1 - Return prisoner 2
+        /// </summary>
+        [TestMethod]
+        public void SaveThePrisoner_FivePrisonersTwoSweetsSeatOne_ReturnsSeatTwo()
+        {
+            // Arrange
+            int prisoners = 5;
+            int sweets = 2;
+            int startingSeat = 1;
+
+            // Assert
+            Assert.AreEqual(2, Solution.SaveThePrisoner(prisoners, sweets, startingSeat));
+        }
+
+        /// <summary>
+        /// Given 7 prisoners, 19 sweets and starting seat 2 - Return prisoner 6
+        /// </summary>
+        [TestMethod]
+        public void SaveThePrisoner_SevenPrisonersNineteenSweetsSeatTwo_ReturnsSeatSix()
+        {
+            // Arrange
+            int prisoners = 7;
+            int sweets = 19;
+            int startingSeat = 2;
+
+            // Assert
+            Assert.AreEqual(6, Solution.SaveThePrisoner(prisoners, sweets, startingSeat));
+        }
+
+        /// <summary>
+        /// Given 352926151 prisoners, 380324688 sweets and starting seat 94730870 - Return prisoner 122129406
+        /// </summary>
+        [TestMethod]
+        public void SaveThePrisoner_LargeNumbers_ReturnsCorrectSeat()
+        {
+            // Arrange
+            int prisoners = 352926151;
+            int sweets = 380324688;
+            int startingSeat = 94730870;
+
+            // Assert
+            Assert.AreEqual(122129406, Solution.SaveThePrisoner(prisoners, sweets, startingSeat));
+        }
+    }
+}

3c85bb16809d4b1bf1eb9b3a55a1451305906dc8
Kareem El-Wakeel <elwakeel91@gmail.com>
2018-09-24 11:08:17 +0100

    Add viral advertising problem

---
+++ Algorithms/Implementation/Viral_Advertising/viral_advertising.cs
@@ -0,0 +1,46 @@
+﻿// URL of the problem
+// https://www.hackerrank.com/challenges/strange-advertising/problem
+
+using System;
+using System.IO;
+
+public class Solution
+{
+
+    static void Main(String[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int result = ViralAdvertising(n);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+
+    public static int ViralAdvertising(int n)
+    {
+        // Number of shares
+        int shares = 5;
+
+        // Running total of likes
+        int total = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            // Get the number of likes that day
+            int likes = shares / 2;
+
+            // Add it to our total
+            total += likes;
+
+            // Calculate the number of shares that day
+            shares = likes * 3;
+        }
+
+        return total;
+    }
+}
---
+++ Algorithms/Implementation/Viral_Advertising/viral_advertising_UnitTests.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HackerRank.UnitTests
+{
+    [TestClass]
+    public class ProgramTest
+    {
+        [TestMethod]
+        public void ViralAdvertising_DayOne_ReturnsTwo()
+        {
+            // Assert
+            Assert.AreEqual(2, Solution.ViralAdvertising(1));
+        }
+
+        [TestMethod]
+        public void ViralAdvertising_DayThree_ReturnsNine()
+        {
+            // Assert
+            Assert.AreEqual(9, Solution.ViralAdvertising(3));
+        }
+    }
+}
